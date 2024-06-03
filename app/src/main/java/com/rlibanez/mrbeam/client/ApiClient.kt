fa55c8ef5dab5789c0ab5c875764476b9b9e930a
rlibanez <rlibanezz@gmail.com>
2024-06-03 19:41:58 +0200

    Add API Client with sectionClient

---
+++ app/src/main/java/com/rlibanez/mrbeam/client/ApiClient.kt
@@ -0,0 +1,19 @@
+package com.rlibanez.mrbeam.client
+
+import com.rlibanez.mrbeam.service.SectionService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val BASE_URL = "https://metalcompendium.duckdns.org/api/"
+
+    fun sectionClient(): SectionService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SectionService::class.java)
+    }
+
+}
