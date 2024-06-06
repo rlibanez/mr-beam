f999e93cdba708ef9b41379298ef69bf3a962e6b
rlibanez <rlibanezz@gmail.com>
2024-06-06 23:52:58 +0200

    Beam screen with two DropdownMenu

--- app/src/main/java/com/rlibanez/mrbeam/ui/beam/BeamActivity.kt
+++ app/src/main/java/com/rlibanez/mrbeam/ui/beam/BeamActivity.kt
@@ -29,6 +29,9 @@ import com.rlibanez.mrbeam.ui.theme.MrBeamTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import com.rlibanez.mrbeam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,35 +43,49 @@ fun BeamActivity(navController: NavHostController) {
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+                .padding(8.dp)
                 .padding(WindowInsets.statusBars.asPaddingValues())
+            //horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Ejemplo de DropdownMenu", style = MaterialTheme.typography.headlineMedium)
-            Spacer(modifier = Modifier.height(16.dp))
-            SimpleDropdownMenuExample()
-            Spacer(modifier = Modifier.height(32.dp))
-
             Text(
-                text = "Ejemplo de ExposedDropdownMenuBox",
-                style = MaterialTheme.typography.headlineMedium
+                text = "Configuración de apoyos",
+                style = TextStyle(fontSize = 20.sp),
+                color = MaterialTheme.colorScheme.primary
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            //Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                //verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .padding(WindowInsets.statusBars.asPaddingValues())
+            ) {
+                SimpleDropdownMenuExample("izquierda")
+                SimpleDropdownMenuExample("derecha")
+            }
         }
     }
 }
 
 @Composable
-fun SimpleDropdownMenuExample() {
+fun SimpleDropdownMenuExample(lado: String) {
     var expanded by remember { mutableStateOf(false) }
-    var selectedOption by remember { mutableStateOf("Selecciona una opción") }
+    //var selectedOption by remember { mutableStateOf("Selecciona una opción") }
     var selectedOptionIndex by remember { mutableStateOf(0) }
-    val options = listOf("Opción 1", "Opción 2", "Opción 3")
-    val images = listOf(
+    val options = listOf("Cero", "Uno", "Dos", "Tres")
+    var images = listOf(
         painterResource(id = R.drawable.cero),
-        painterResource(id = R.drawable.uno),
+        painterResource(id = R.drawable.unoi),
         painterResource(id = R.drawable.dos),
         painterResource(id = R.drawable.tres))
+    if(lado == "derecha") {
+        images = listOf(
+            painterResource(id = R.drawable.cero),
+            painterResource(id = R.drawable.unod),
+            painterResource(id = R.drawable.dos),
+            painterResource(id = R.drawable.tres))
+    }
 
     Box {
         Button(
