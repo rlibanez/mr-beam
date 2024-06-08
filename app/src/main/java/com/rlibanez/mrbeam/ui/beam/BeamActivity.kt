d18963fade3d3331d71c02b9eba81fca896434fe
rlibanez <rlibanezz@gmail.com>
2024-06-08 17:25:16 +0200

    Add supports png and odg

--- app/src/main/java/com/rlibanez/mrbeam/ui/beam/BeamActivity.kt
+++ app/src/main/java/com/rlibanez/mrbeam/ui/beam/BeamActivity.kt
@@ -1,40 +1,46 @@
 package com.rlibanez.mrbeam.ui.beam
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.rlibanez.mrbeam.R
 import com.rlibanez.mrbeam.ui.theme.MrBeamTheme
 
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
-import com.rlibanez.mrbeam.R
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BeamActivity(navController: NavHostController) {
     Surface(
@@ -43,6 +49,7 @@ fun BeamActivity(navController: NavHostController) {
     ) {
         Column(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(8.dp)
                 .padding(WindowInsets.statusBars.asPaddingValues())
             //horizontalAlignment = Alignment.CenterHorizontally
@@ -52,17 +59,21 @@ fun BeamActivity(navController: NavHostController) {
                 style = TextStyle(fontSize = 20.sp),
                 color = MaterialTheme.colorScheme.primary
             )
-            //Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
             Row(
                 //verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(16.dp)
-                    .padding(WindowInsets.statusBars.asPaddingValues())
+                    //.padding(4.dp)
+                    //.padding(WindowInsets.statusBars.asPaddingValues())
             ) {
-                SimpleDropdownMenuExample("izquierda")
-                SimpleDropdownMenuExample("derecha")
+                Box(modifier = Modifier.offset(x = 2.dp)) {
+                    SimpleDropdownMenuExample("izquierda")
+                }
+                Box(modifier = Modifier.offset(x = (-2).dp)) {
+                    SimpleDropdownMenuExample("derecha")
+                }
             }
         }
     }
@@ -71,27 +82,26 @@ fun BeamActivity(navController: NavHostController) {
 @Composable
 fun SimpleDropdownMenuExample(lado: String) {
     var expanded by remember { mutableStateOf(false) }
-    //var selectedOption by remember { mutableStateOf("Selecciona una opción") }
-    var selectedOptionIndex by remember { mutableStateOf(0) }
+    var selectedOptionIndex by remember { mutableIntStateOf(0) }
     val options = listOf("Cero", "Uno", "Dos", "Tres")
     var images = listOf(
-        painterResource(id = R.drawable.cero),
-        painterResource(id = R.drawable.unoi),
-        painterResource(id = R.drawable.dos),
-        painterResource(id = R.drawable.tres))
+        painterResource(id = R.drawable.support0i),
+        painterResource(id = R.drawable.support1i),
+        painterResource(id = R.drawable.support2i),
+        painterResource(id = R.drawable.support3i))
     if(lado == "derecha") {
         images = listOf(
-            painterResource(id = R.drawable.cero),
-            painterResource(id = R.drawable.unod),
-            painterResource(id = R.drawable.dos),
-            painterResource(id = R.drawable.tres))
+            painterResource(id = R.drawable.support0d),
+            painterResource(id = R.drawable.support1d),
+            painterResource(id = R.drawable.support2d),
+            painterResource(id = R.drawable.support3d))
     }
 
     Box {
         Button(
             onClick = { expanded = true },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent), // Configuración del color de fondo transparente
-            contentPadding = PaddingValues(0.dp), // Eliminar el relleno del contenido del botón
+            contentPadding = PaddingValues(2.dp), // Eliminar el relleno del contenido del botón
             elevation = null, // Desactivar la elevación del botón
             modifier = Modifier.wrapContentSize() // Hacer que el tamaño del botón sea el tamaño del contenido
 
@@ -101,15 +111,15 @@ fun SimpleDropdownMenuExample(lado: String) {
                 painter = images[selectedOptionIndex],
                 contentDescription = null,
                 modifier = Modifier
-                    .size(150.dp)
-                    .padding(end = 6.dp)
+                    .size(175.dp)
+                    .padding(0.dp)
             )
         }
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
         ) {
-            options.forEachIndexed { index, option ->
+            options.forEachIndexed { index, _ ->
                 DropdownMenuItem(
                     text = {
                         Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
@@ -118,9 +128,8 @@ fun SimpleDropdownMenuExample(lado: String) {
                                 contentDescription = null,
                                 modifier = Modifier
                                     .size(150.dp)
-                                    .padding(end = 6.dp)
+                                    //.padding(end = 0.dp)
                             )
-                            //Text(option)
                         }
                     },
                     onClick = {
